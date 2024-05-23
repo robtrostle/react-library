cc040f5d7222dde3b9b3877270385c87de78ca78
Robert Trostle <51966615+robtrostle@users.noreply.github.com>
2024-05-23 09:25:44 -0400

    Library Services and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
 import { Carousel } from './layouts/HomePage/Carousel';
 import { Heros } from './layouts/HomePage/Heros';
+import { LibraryService } from './layouts/HomePage/LibraryServices';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
     <ExploreTopBooks/>
     <Carousel/>
     <Heros/>
+    <LibraryService/>
+    <Footer/>
     </div>
   );
 }
