7cd3921c75170a8e75b082776f7e380bbfa1fcd0
Rob Trostle <rtrostle@nipr.com>
2024-07-04 10:58:27 -0400

    implement checkout functionality

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -5,19 +5,28 @@ import { StarsReview } from "../Utils/StarsReview";
 import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
 import ReviewModel from "../../models/ReviewModel";
 import { LatestReviews } from "./LatestReviews";
+import { useOktaAuth } from "@okta/okta-react";
 
 export const BookCheckoutPage = () => {
+
+    const { authState } = useOktaAuth();
 
     const [book, setBook] = useState<BookModel>();
     const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
 
     //Review State
-    
     const [reviews, setReviews] = useState<ReviewModel[]>([]);
     const [totalStars, setTotalStars] = useState(0);
     const [isLoadingReview, setIsLoadingReview] = useState(true);
 
+    //Loans Count State
+    const [currentLoansCount, setCurrentLoansCount] = useState(0);
+    const [isLoadingCurrentLoansCount, setIsLoadingCurrentLoansCount] = useState(true);
+
+    // Is Book Check Out?
+    const [isCheckedOut, setIsCheckedOut] = useState(false);
+    const [isLoadingBookCheckedOut, setIsLoadingBookCheckedOut] = useState(true);
 
     const bookId = (window.location.pathname).split('/')[2];
 
@@ -51,7 +60,7 @@ export const BookCheckoutPage = () => {
             setIsLoading(false);
             setHttpError(error.message);
         })
-    }, []);
+    }, [isCheckedOut]);
 
     useEffect(() => {
         const fetchBookReviews = async () => {
@@ -96,8 +105,62 @@ export const BookCheckoutPage = () => {
         });
     },[]);
    
+    useEffect(() => {
+        const fetchUserCurrentLoansCount = async () => {
+            if (authState && authState.isAuthenticated) {
+                const url = `http://localhost:8080/api/books/secure/currentloans/count`;
+                const requestOptions = {
+                    method: 'GET',
+                    headers: { 
+                        Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                        'Content-Type': 'application/json'
+                     }
+                };
+                const currentLoansCountResponse = await fetch(url, requestOptions);
+                if (!currentLoansCountResponse.ok)  {
+                    throw new Error('Something went wrong!');
+                }
+                const currentLoansCountResponseJson = await currentLoansCountResponse.json();
+                setCurrentLoansCount(currentLoansCountResponseJson);
+            }
+            setIsLoadingCurrentLoansCount(false);
+        }
+        fetchUserCurrentLoansCount().catch((error: any) => {
+            setIsLoadingCurrentLoansCount(false);
+            setHttpError(error.message);
+        })
+    }, [authState, isCheckedOut]);
 
-    if (isLoading || isLoadingReview) {
+    useEffect(() => {
+        const fetchUserCheckedOutBook = async () => {
+            if (authState && authState.isAuthenticated) {
+                const url = `http://localhost:8080/api/books/secure/ischeckedout/byuser?bookId=${bookId}`;
+                const requestOptions = {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                        'Content-Type': 'application/json'
+                    }
+                };
+                const bookCheckedOut = await fetch(url, requestOptions);
+
+                if (!bookCheckedOut.ok) {
+                    throw new Error('Something went wrong!');
+                }
+
+                const bookCheckedOutResponseJson = await bookCheckedOut.json();
+                setIsCheckedOut(bookCheckedOutResponseJson);
+            }
+            setIsLoadingBookCheckedOut(false);
+        }
+        fetchUserCheckedOutBook().catch((error: any) => {
+            setIsLoadingBookCheckedOut(false);
+            setHttpError(error.message);
+        })
+    }, [authState]);
+
+
+    if (isLoading || isLoadingReview || isLoadingCurrentLoansCount || isLoadingBookCheckedOut) {
         return (
             <SpinnerLoading />
         )
@@ -109,6 +172,23 @@ export const BookCheckoutPage = () => {
                 <p>{httpError}</p>
             </div>
         )
+    }
+
+    async function checkoutBook() {
+        //possible error remove slash after checkout
+        const url = `http://localhost:8080/api/books/secure/checkout?bookId=${book?.id}`;
+        const requestOptions = {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        };
+        const checkoutResponse = await fetch(url, requestOptions);
+        if (!checkoutResponse.ok) {
+            throw new Error('Something went wrong!');
+        }
+        setIsCheckedOut(true);
     }
 
     return (
@@ -131,7 +211,8 @@ export const BookCheckoutPage = () => {
                             <StarsReview rating={totalStars} size={32} />
                         </div>
                     </div>
-                    <CheckoutAndReviewBox book={book} mobile={false} />
+                    <CheckoutAndReviewBox book={book} mobile={false} currentLoansCount={currentLoansCount}
+                    isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut} checkoutBook={checkoutBook}/>
                 </div>
                 <hr />
                 <LatestReviews reviews={reviews} bookId={book?.id} mobile={false} />
@@ -153,7 +234,8 @@ export const BookCheckoutPage = () => {
                         <StarsReview rating={totalStars} size={32} />
                     </div>
                 </div>
-                <CheckoutAndReviewBox book={book} mobile={true} />
+                <CheckoutAndReviewBox book={book} mobile={true} currentLoansCount={currentLoansCount}
+                 isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut} checkoutBook={checkoutBook} />
                 <hr />
                 <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
             </div>
--- src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
+++ src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
@@ -1,45 +1,61 @@
 import { Link } from "react-router-dom";
 import BookModel from "../../models/BookModel";
 
-export const CheckoutAndReviewBox: React.FC<{ book: BookModel | undefined, mobile: boolean, }> = (props) => {
-    return (
-        <div className={props.mobile ? 'card d-flex mt-5' : 'card col-3 container d-flex mb-5'}>
-            <div className='card-body container'>
-                <div className='mt-3'>
-                    <p>
-                        <b>0/5 </b>
-                        books checked out
-                    </p>
-                    <hr />
-                    {props.book && props.book.copiesAvailable && props.book.copiesAvailable > 0 ?
-                        <h4 className='text-success'>
-                            Available
-                        </h4>
-                        :
-                        <h4 className='text-danger'>
-                            Wait List
-                        </h4>
-                    }
-                    <div className='row'>
-                        <p className='col-6 lead'>
-                            <b>{props.book?.copies} </b>
-                            copies
+export const CheckoutAndReviewBox: React.FC<{ book: BookModel | undefined, mobile: boolean,
+    currentLoansCount: number, isAuthenticated: any, isCheckedOut: boolean,
+    checkoutBook: any}> = (props) => {
+
+        function buttonRender() {
+            if (props.isAuthenticated) {
+                if (!props.isCheckedOut && props.currentLoansCount < 5) {
+                    return (<button onClick={() => props.checkoutBook()} className="btn btn-success btn-lg">Checkout</button>)
+                } else if (props.isCheckedOut) {
+                    return (<p>Book checked out. Enjoy!</p>)
+                } else if (!props.isCheckedOut){
+                    return (<p className="text-danger">Too many books checked out.</p>)
+                }
+            }
+            return (<Link to={'/login'} className="btn btn-success btn-lg">Sign in</Link>)
+        }
+
+        return (
+            <div className={props.mobile ? 'card d-flex mt-5' : 'card col-3 container d-flex mb-5'}>
+                <div className='card-body container'>
+                    <div className='mt-3'>
+                        <p>
+                            <b>{props.currentLoansCount}/5 </b>
+                            books checked out
                         </p>
-                        <p className='col-6 lead'>
-                            <b>{props.book?.copiesAvailable} </b>
-                            available
-                        </p>
+                        <hr />
+                        {props.book && props.book.copiesAvailable && props.book.copiesAvailable > 0 ?
+                            <h4 className='text-success'>
+                                Available
+                            </h4>
+                            :
+                            <h4 className='text-danger'>
+                                Wait List
+                            </h4>
+                        }
+                        <div className='row'>
+                            <p className='col-6 lead'>
+                                <b>{props.book?.copies} </b>
+                                copies
+                            </p>
+                            <p className='col-6 lead'>
+                                <b>{props.book?.copiesAvailable} </b>
+                                available
+                            </p>
+                        </div>
                     </div>
+                    {buttonRender()}
+                    <hr />
+                    <p className='mt-3'>
+                        This number can change until placing order has been complete.
+                    </p>
+                    <p>
+                        Sign in to be able to leave a review.
+                    </p>
                 </div>
-                <Link to='/#' className='btn btn-success btn-lg'>Sign in</Link>
-                <hr />
-                <p className='mt-3'>
-                    This number can change until placing order has been complete.
-                </p>
-                <p>
-                    Sign in to be able to leave a review.
-                </p>
             </div>
-        </div>
-    );
-}
+        );
+    }
