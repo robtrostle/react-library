3bbf35d7ee0e02e45bc468892f9fa43f13b52e9f
Robert Trostle <51966615+robtrostle@users.noreply.github.com>
2024-07-25 16:13:54 -0400

    trying some stuff 3

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oahxfqoq5rBpyQuI5d7',
     issuer: 'https://dev-36347935.okta.com/oauth2/default',
-    redirectUri: 'http//localhost:3000/login/callback',
+    redirectUri: 'https://master.d29o4ngnjdpteo.amplifyapp.com/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
