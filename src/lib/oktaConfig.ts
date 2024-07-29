f19ebd8318332be339c14f12eb35aeae5f687116
Robert Trostle <51966615+robtrostle@users.noreply.github.com>
2024-07-29 10:23:48 -0400

    adding a branch for local testing

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oahxfqoq5rBpyQuI5d7',
     issuer: 'https://dev-36347935.okta.com/oauth2/default',
-    redirectUri: 'https://master.d29o4ngnjdpteo.amplifyapp.com/login/callback',
+    redirectUri: 'http://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
